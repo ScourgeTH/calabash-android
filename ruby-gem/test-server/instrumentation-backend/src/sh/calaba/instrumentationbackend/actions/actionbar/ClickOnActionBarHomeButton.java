cdd80fa147cdbde0ca40f186caa9050d25561942
John Wells <einstn99@fast-mail.org>
2013-01-08 14:04:54 -0500

    Added click actionbarhomebutton action

---
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/actionbar/ClickOnActionBarHomeButton.java
@@ -0,0 +1,23 @@
+package sh.calaba.instrumentationbackend.actions.actionbar;
+
+import sh.calaba.instrumentationbackend.InstrumentationBackend;
+import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.actions.Action;
+
+public class ClickOnActionBarHomeButton implements Action {
+
+	@Override
+	public Result execute(String... args) {
+		
+		InstrumentationBackend.solo.clickOnActionBarHomeButton();
+		
+		return Result.successResult();
+	}
+
+	@Override
+	public String key() {
+		
+		return "click_on_action_bar_home_button";
+	}
+
+}
